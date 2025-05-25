fe7e9cf20b8ca7111509684c8cb431fae4e076fc
Mohammad Arok <121833815+mohammad-ahmad-arok@users.noreply.github.com>
2025-05-25 11:58:34 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 db.connect(err => {
